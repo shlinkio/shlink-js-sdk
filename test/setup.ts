a10cf334e840ab9ef51bd2b05bfa49ceb541069f
Alejandro Celaya <alejandrocelaya@gmail.com>
2025-02-08 10:46:06 +0100

    Remove dependency on testing library packages

--- test/setup.ts
+++ test/setup.ts
@@ -1,9 +1,6 @@
-import '@testing-library/jest-dom/vitest';
-import { cleanup } from '@testing-library/react';
 import { afterEach } from 'vitest';
 
 // Clears all mocks and cleanup DOM after every test
 afterEach(() => {
   vi.clearAllMocks();
-  cleanup();
 });
