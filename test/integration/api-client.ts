0d61540ec6e0e5cf53be6e37e19f834bb0ee711f
Alejandro Celaya <alejandrocelaya@gmail.com>
2025-02-09 11:24:38 +0100

    Run integration tests in node, deno and bun

--- test/integration/api-client.ts
+++ test/integration/api-client.ts
@@ -1,12 +1,12 @@
 import { ShlinkApiClient } from '../../src';
-import { NodeHttpClient } from '../../src/node';
+import { FetchHttpClient } from '../../src/fetch';
 
 const apiKey = process.env.SHLINK_API_KEY;
 if (!apiKey) {
   throw new Error('API key not found in SHLINK_API_KEY env');
 }
 
-export const apiClient = new ShlinkApiClient(new NodeHttpClient(), {
+export const apiClient = new ShlinkApiClient(new FetchHttpClient(), {
   baseUrl: 'http://localhost:8765',
   apiKey,
 });
