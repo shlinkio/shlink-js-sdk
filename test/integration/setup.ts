c850d5a988a4d54086810a8798f6fb0cffd4ebe8
Alejandro Celaya <alejandrocelaya@gmail.com>
2025-02-12 09:20:59 +0100

    Do not try to delete orphan visits in integration tests for SHlink <3.6.0

--- test/integration/setup.ts
+++ test/integration/setup.ts
@@ -1,6 +1,17 @@
 import { afterEach } from 'vitest';
 import { apiClient } from './api-client';
 
+function padArray<T>(arr: T[], length: number, fillValue: T) {
+  if (arr.length >= length) {
+    return arr;
+  }
+
+  const padLength = length - arr.length;
+  const padArray = new Array(padLength).fill(fillValue);
+
+  return [...arr, ...padArray];
+}
+
 afterEach(async () => {
   vi.clearAllMocks();
 
@@ -9,9 +20,13 @@ afterEach(async () => {
     apiClient.listShortUrls({ itemsPerPage: 100 }),
     apiClient.listTags(),
   ]);
+
+  const version = process.env.SHLINK_VERSION ?? '';
+  const normalizedVersion = padArray(version.split('.'), 3, '0').join('.');
+
   await Promise.all([
     ...shortUrls.data.map((shortUrl) => apiClient.deleteShortUrl(shortUrl)),
     tags.data.length > 0 ? apiClient.deleteTags(tags.data) : Promise.resolve(),
-    apiClient.deleteOrphanVisits(),
+    normalizedVersion >= '3.6.0' ? apiClient.deleteOrphanVisits() : Promise.resolve(),
   ]);
 });
